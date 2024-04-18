49d7077c95cfabd2b1f91775489c415d88b5d0d8
gigawa77 <rpamzn@gmail.com>
2024-04-18 12:23:44 +0100

    completed task 4

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -52,3 +52,54 @@ describe("/api/topics", () => {
       });
   });
 });
+
+describe("/api/articles/:article_id", () => {
+  test("GET 200: Should respond with the correct article", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        expect(article).toEqual([
+          {
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: "2020-07-09T20:11:00.000Z",
+            votes: 100,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          },
+        ]);
+      });
+  });
+  test("GET 404: Should return a 404 when given an article id that doesn't exist", () => {
+    return request(app)
+      .get("/api/articles/999")
+      .expect(404)
+      .then(({ body }) => {
+        const { message } = body;
+        expect(message).toBe("article not found");
+      });
+  });
+  test("GET 404: Should return a 404 when given an invalid endpoint", () => {
+    return request(app)
+      .get("/api/article/1")
+      .expect(404)
+      .then(({ body }) => {
+        const { message } = body;
+        expect(message).toBe("Endpoint not found");
+      });
+  });
+  test("GET 400: Should return a 400 when given an invalid id", () => {
+    return request(app)
+      .get("/api/articles/invalid")
+      .expect(400)
+      .then(({ body }) => {
+        const { message } = body;
+        expect(message).toBe("Bad request");
+      });
+  });
+});
