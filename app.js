058363ae039112e1e84ae0315d59144faf8e993e
gigawa77 <rpamzn@gmail.com>
2024-04-19 11:50:00 +0100

    completed task 5

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const {
   getHealthCheck,
   getTopics,
   getEndpoints,
+  getArticleId,
+  getAllArticles,
 } = require("./controllers/controller");
 
 const app = express();
-
-// app.use(express.json());
 
 app.get("/api/healthcheck", getHealthCheck);
 
@@ -16,8 +16,24 @@ app.get("/api/topics", getTopics);
 
 app.get("/api", getEndpoints);
 
+app.get("/api/articles/:article_id", getArticleId);
+
+app.get("/api/articles", getAllArticles);
+
 app.all("*", (req, res, next) => {
   res.status(404).send({ message: "Endpoint not found" });
 });
 
+app.use((err, req, res, next) => {
+  if (err.status && err.message) {
+    res.status(err.status).send({ message: err.message });
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "Bad request" });
+  }
+});
 module.exports = app;
--- models/model.js
+++ models/model.js
@@ -10,3 +10,34 @@ exports.getTopicData = () => {
       return rows;
     });
 };
+
+exports.getCorrectArticle = (id) => {
+  return db
+    .query(
+      `
+  SELECT * FROM articles
+  WHERE article_id = $1`,
+      [id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, message: "article not found" });
+      }
+      return rows;
+    });
+};
+
+exports.getArticles = () => {
+  return db
+    .query(
+      `
+      SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count
+      FROM articles
+      LEFT JOIN comments ON articles.article_id = comments.article_id
+      GROUP BY articles.article_id;
+    `
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
